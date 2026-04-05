50c69682d9bc310829817f0c7d008cd824cf7b16
OusmaneGith <ousmane.dialloh1@gmail.com>
2026-04-05 17:36:21 +0200

    Parties 2&3 : codage en mémoire et affichage intégrés à l'analyseur

--- src/polynomes/Monome.java
+++ src/polynomes/Monome.java
@@ -1,5 +1,13 @@
 package polynomes;
 
 public class Monome {
+    double coefficient;
+    int    exposant;
+    Monome suivant;
 
+    public Monome(double coefficient, int exposant) {
+        this.coefficient = coefficient;
+        this.exposant    = exposant;
+        this.suivant     = null;
+    }
 }
